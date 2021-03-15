a56759854792ee21aeb7c76d4c3e4b1a134f1fbe
Vikas Bhalla <77879682+Vikas0004@users.noreply.github.com>
2021-03-15 16:01:30 +0530

    adding package

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/',(req , res)=>{
-	res.send(database.users)
+	res.send("its working")
 })
 
 // app.get('/profile/:id' , (req , res) => { profile.profileHandler(req , res , db) } )
